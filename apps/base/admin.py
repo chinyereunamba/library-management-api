f57700b33c9da7df4a80125415dfd8d79a40020e
Chinyere <chinyereunamba17@gmail.com>
2023-09-09 02:11:14 +0100

    add book instance model

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
-from .models import Author, Book, Genre, Transaction
+from .models import Author, Book, BookInstance, Genre, Transaction
 
 # Register your models here.
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ["title",'quantity', "genre"]
+    list_display = ["title", "quantity", "genre"]
     search_fields = ["title", "genre"]
 
+
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ['book', 'user', 'admin']
-    search_fields = ['book', 'user']
+    list_display = ["book", "user", "admin"]
+    search_fields = ["book", "user"]
+
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance)
 admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Genre)
 admin.site.register(Author)
--- apps/base/models.py
+++ apps/base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.accounts.models import Account, Admin
+import uuid
 
 # Create your models here.
 
@@ -22,22 +23,57 @@ class Book(models.Model):
     author = models.CharField(max_length=80, blank=False, null=False)
     title = models.CharField(max_length=300, blank=False, null=False)
     isbn = models.CharField(verbose_name="ISBN", max_length=20, blank=True, null=True)
+    summary = models.TextField(blank=True)
     date_added = models.DateField(auto_now_add=True)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     quantity = models.IntegerField(verbose_name="Quantity of books", default=5)
 
+    class Meta:
+        ordering = ["title"]
+
+
+# LOAN_STATUS = [("borrowed", "Borrowed"), ("returned", "Returned")]
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200, blank=True)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
 
 class Transaction(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     admin = models.ForeignKey(Admin, on_delete=models.DO_NOTHING)
-    book = models.ForeignKey(Book, on_delete=models.DO_NOTHING)
+    book = models.ForeignKey(BookInstance, on_delete=models.DO_NOTHING)
     date_borrowed = models.DateTimeField(
         verbose_name="date the book was borrowed",
         auto_now_add=True,
         blank=True,
         null=True,
     )
-    date_returned = models.DurationField()
 
     def __str__(self):
-        return f'{self.book} was borrowed by {self.user}'
+        return f"{self.book} was borrowed by {self.user}"
+
